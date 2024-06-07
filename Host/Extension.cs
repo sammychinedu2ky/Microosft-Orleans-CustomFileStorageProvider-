bfc2210ae22211d37514225300845f2c67a57690
Samson Amaugo <sammychinedu2ky@gmail.com>
2024-06-07 04:48:35 +0100

    extension added

---
+++ Host/Extension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Storage;
+
+namespace Host
+{
+    public class FileConfigurationOptions
+    {
+        public string? StorePath { get; set; }
+        public const string MyFileConfiguration = "FileConfiguration";
+    }
+
+    public static class FileStorageExtensions
+    {
+        public static IServiceCollection AddFileStorage(this IServiceCollection services, Action<FileConfigurationOptions> configureOptions)
+        {
+            services.AddOptions<FileConfigurationOptions>()
+                       .Configure(configureOptions);
+            return services.AddKeyedSingleton<IGrainStorage, FileStorage>("fileStateStore");
+
+        }
+
+        public static IServiceCollection AddFileStorage(this IServiceCollection services)
+        {
+            services.AddOptions<FileConfigurationOptions>()
+               .Configure<IConfiguration>((settings, configuration) =>
+               {
+                   configuration.GetSection(FileConfigurationOptions.MyFileConfiguration).Bind(settings);
+               });
+            return services.AddKeyedSingleton<IGrainStorage, FileStorage>("fileStateStore");
+        }
+
+    }
+}
--- Host/Program.cs
+++ Host/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Host;
+using Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Orleans.Runtime;
+using Orleans.Storage;
+using System.Text.Json;
+
+IHostBuilder builder = new HostBuilder()
+    .UseOrleans(silo =>
+    {
+        silo.UseLocalhostClustering();
+        silo.Services.AddFileStorage();
+    });
+
+builder.ConfigureAppConfiguration((context, config) =>
+{
+    config.AddJsonFile("json_config_path", optional: true);
+});
+using IHost host = builder.Build();
+
+await host.RunAsync();
+
+public class PersonGrain : IGrainBase, IPersonGrain
+{
+    IPersistentState<PersonState> state;
+    public IGrainContext GrainContext { get; }
+    public PersonGrain(IGrainContext context, [PersistentState("personState", "fileStateStore")] IPersistentState<PersonState> state) => (GrainContext, this.state) = (context, state);
+    public async Task AddName(string name)
+    {
+        var context = this.GrainContext;
+        state.State.Name = name;
+        await state.WriteStateAsync();
+    }
+    public Task<string> GetName()
+    {
+        return Task.FromResult($"Your name is {state.State.Name}");
+    }
+}
+
+
+[GenerateSerializer]
+public class PersonState
+{
+    public string? Name { get; set; }
+}
+
+public class FileStorage : IGrainStorage
+{
+    private void _ensureFileExists(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            var directory = Path.GetDirectoryName(filePath);
+            Directory.CreateDirectory(directory!);
+            File.Create(filePath).Close();
+        }
+    }
+
+    private string _getFilePath(GrainId grainId, string stateName)
+    {
+        return Path.Combine(_options.Value.StorePath!, grainId.Type.ToString()!, grainId.Key.ToString()!, stateName + ".json");
+    }
+    private readonly IOptions<FileConfigurationOptions> _options;
+    public FileStorage(IOptions<FileConfigurationOptions> options) => _options = options;
+    public Task ClearStateAsync<T>(string stateName, GrainId grainId, IGrainState<T> grainState)
+    {
+        var path = _getFilePath(grainId, stateName);
+        File.Delete(path);
+        return Task.CompletedTask;
+    }
+
+    public async Task ReadStateAsync<T>(string stateName, GrainId grainId, IGrainState<T> grainState)
+    {
+        var path = _getFilePath(grainId, stateName);
+        _ensureFileExists(path);
+        var data = await File.ReadAllTextAsync(path);
+        if (string.IsNullOrEmpty(data)) return;
+        grainState.State = JsonSerializer.Deserialize<T>(data)!;
+    }
+
+    public async Task WriteStateAsync<T>(string stateName, GrainId grainId, IGrainState<T> grainState)
+    {
+        var path = _getFilePath(grainId, stateName);
+        _ensureFileExists(path);
+        var data = JsonSerializer.Serialize(grainState.State);
+        await File.WriteAllTextAsync(path, data);
+    }
+}
