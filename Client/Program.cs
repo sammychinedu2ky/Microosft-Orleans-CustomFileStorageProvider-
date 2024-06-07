29fecff683ac4c6b26e4e87228b1fbb0fabc8d6f
Samson Amaugo <sammychinedu2ky@gmail.com>
2024-06-07 09:39:43 +0100

    clean init

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,38 +12,10 @@ await host.StartAsync();
 IClusterClient client = host.Services.GetRequiredService<IClusterClient>();
 var grain1 = client.GetGrain<IPersonGrain>("sam1");
 var grain2 = client.GetGrain<IPersonGrain>("sam1");
-Task.Run(async () =>
-{
-    try
-    {
-        
-    await grain2.AddName(new string('a', 100000));
-        Console.WriteLine("a done");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"from a");
-        Console.WriteLine(ex.Message);
-    }
-});
-Task.Run(async () =>
-{
-    try
-    {
-
-        await grain2.AddName(new string('b', 1000000));
-        Console.WriteLine("b done");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"from b");
-        Console.WriteLine(ex.Message);
-    }
-});
-await Task.Delay(1000);
-await grain2.AddName(new string('c', 1000000));
-Console.WriteLine("c done");
+await grain1.AddName("Samson Amaugo");
+await grain2.AddName("Swacblooms");
 var name1 = await grain1.GetName();
 var name2 = await grain2.GetName();
-
+Console.WriteLine(name1);
+Console.WriteLine(name2);
 Console.ReadLine();
